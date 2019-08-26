abf9263aed52cb1b2ba59008925e5a3f26b9485a
Beni Ben Zikry <bbenzikry@gmail.com>
2019-08-26 23:37:09 +0300

    Added success icons for exit_code section

--- sections/exit_code.zsh
+++ sections/exit_code.zsh
@@ -11,6 +11,8 @@ SPACESHIP_EXIT_CODE_SHOW="${SPACESHIP_EXIT_CODE_SHOW=false}"
 SPACESHIP_EXIT_CODE_PREFIX="${SPACESHIP_EXIT_CODE_PREFIX=""}"
 SPACESHIP_EXIT_CODE_SUFFIX="${SPACESHIP_EXIT_CODE_SUFFIX=" "}"
 SPACESHIP_EXIT_CODE_SYMBOL="${SPACESHIP_EXIT_CODE_SYMBOL="✘"}"
+SPACESHIP_EXIT_CODE_OK_SYMBOL="${SPACESHIP_EXIT_CODE_OK_SYMBOL="✔"}"
+
 SPACESHIP_EXIT_CODE_COLOR="${SPACESHIP_EXIT_CODE_COLOR="red"}"
 
 # ------------------------------------------------------------------------------
@@ -18,11 +20,20 @@ SPACESHIP_EXIT_CODE_COLOR="${SPACESHIP_EXIT_CODE_COLOR="red"}"
 # ------------------------------------------------------------------------------
 
 spaceship_exit_code() {
-  [[ $SPACESHIP_EXIT_CODE_SHOW == false || $RETVAL == 0 ]] && return
-
+  # [[ $SPACESHIP_EXIT_CODE_SHOW == false || $RETVAL == 0 ]] && return
+  [[ $SPACESHIP_EXIT_CODE_SHOW == false ]] && return
+  local EXIT_CODE="$RETVAL"
+  local EXIT_CODE_SNIPPET=""
+  if [[ "$EXIT_CODE" == 0 ]] 
+  then
+    EXIT_CODE_SNIPPET="${SPACESHIP_EXIT_CODE_OK_SYMBOL}"
+    SPACESHIP_EXIT_CODE_COLOR="green"
+  else
+    EXIT_CODE_SNIPPET="${SPACESHIP_EXIT_CODE_SYMBOL}${EXIT_CODE}" \
+  fi
   spaceship::section \
     "$SPACESHIP_EXIT_CODE_COLOR" \
     "$SPACESHIP_EXIT_CODE_PREFIX" \
-    "${SPACESHIP_EXIT_CODE_SYMBOL}$RETVAL" \
+    "$EXIT_CODE_SNIPPET" \
     "$SPACESHIP_EXIT_CODE_SUFFIX"
 }
